ca9c2caa3780b715f3465c13ee941b9759a2e154
Alex Paul <alexpaulnyc@gmail.com>
2022-02-09 20:07:19 -0500

    Created IAPManager and IAPManagerError

--- FakeGame/Complete/FakeGame/FakeGame/In-App Purchases/IAPManager.swift
+++ FakeGame/Complete/FakeGame/FakeGame/In-App Purchases/IAPManager.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 Appcoda. All rights reserved.
 //
 
-import Foundation
+import StoreKit
+
+class IAPManager: NSObject {
+
+    enum IAPManagerError: Error {
+        case noProductIDsFound
+        case noProductsFound
+        case paymentWasCancelled
+        case productRequestFailed
+    }
+
+    static let shared = IAPManager()
+
+    private override init() {
+        super.init()
+    }
+}
+
+extension IAPManager.IAPManagerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .noProductIDsFound: return "No In-App Purchase product identifiers were found."
+        case .noProductsFound: return "No In-App Purchases were found."
+        case .productRequestFailed: return "Unable to fetch available In-App Purchase products at the moment."
+        case .paymentWasCancelled: return "In-App Purchase process was cancelled."
+        }
+    }
+}
